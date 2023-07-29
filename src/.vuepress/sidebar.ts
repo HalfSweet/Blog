73c34dc6a43180ab5e14c03d28162cd6a58c28ae
HalfSweet <halfsweet@halfsweet.cn>
2023-07-30 00:09:26 +0800

    update: 切换为vuepress-hope主题

---
+++ src/.vuepress/sidebar.ts
@@ -0,0 +1,13 @@
+import { sidebar } from "vuepress-theme-hope";
+
+export default sidebar({
+  "/": [
+    "",
+    {
+      text: "文章",
+      icon: "book",
+      prefix: "posts/",
+      children: "structure",
+    }
+  ],
+});
---
+++ src/.vuepress/theme.ts
@@ -0,0 +1,168 @@
+import { hopeTheme } from "vuepress-theme-hope";
+import navbar from "./navbar.js";
+import sidebar from "./sidebar.js";
+
+export default hopeTheme({
+  hostname: "https://halfsweet.cn",
+
+  author: {
+    name: "HalfSweet",
+    url: "https://github.com/HalfSweet",
+  },
+
+  //iconAssets: "https://halfsweet.cn/img/LOGO.png",
+
+  logo: "/img/Headshot.jpg",
+
+  repo: "HalfSweet/Blog",
+
+  docsDir: "src",
+
+  // navbar
+  navbar,
+
+  // sidebar
+  sidebar,
+
+  footer: "今天咕咕被欺负了吗",
+
+  displayFooter: true,
+
+  blog: {
+    description: "Try to create some value",
+    intro: "/Catalogue/02.about.html",
+    medias: {
+      Email: "mailto:HalfSweet@HalfSweet.cn",
+      GitHub: "https://github.com/HalfSweet",
+      Twitter: "https://twitter.com/_HalfSweet",
+    },
+  },
+
+  // encrypt: {
+  //   config: {
+  //     "/demo/encrypt.html": ["1234"],
+  //   },
+  // },
+
+  // page meta
+  metaLocales: {
+    editLink: "在 GitHub 上编辑此页",
+  },
+
+  plugins: {
+    blog: true,
+
+    // comment: {
+    //   // You should generate and use your own comment service
+    //   provider: "Waline",
+    //   serverURL: "https://waline-comment.vuejs.press",
+    // },
+
+    // all features are enabled for demo, only preserve features you need here
+    mdEnhance: {
+      card: true,
+      align: true,
+      attrs: true,
+      chart: true,
+      codetabs: true,
+      demo: true,
+      echarts: true,
+      figure: true,
+      flowchart: true,
+      gfm: true,
+      imgLazyload: true,
+      imgSize: true,
+      include: true,
+      katex: true,
+      mark: true,
+      mermaid: true,
+      playground: {
+        presets: ["ts", "vue"],
+      },
+      presentation: ["highlight", "math", "search", "notes", "zoom"],
+      stylize: [
+        {
+          matcher: "Recommended",
+          replacer: ({ tag }) => {
+            if (tag === "em")
+              return {
+                tag: "Badge",
+                attrs: { type: "tip" },
+                content: "Recommended",
+              };
+          },
+        },
+      ],
+      sub: true,
+      sup: true,
+      tabs: true,
+      vPre: true,
+      vuePlayground: true,
+    },
+
+    
+
+    // uncomment these if you want a PWA
+    // pwa: {
+    //   favicon: "/favicon.ico",
+    //   cacheHTML: true,
+    //   cachePic: true,
+    //   appendBase: true,
+    //   apple: {
+    //     icon: "/assets/icon/apple-icon-152.png",
+    //     statusBarColor: "black",
+    //   },
+    //   msTile: {
+    //     image: "/assets/icon/ms-icon-144.png",
+    //     color: "#ffffff",
+    //   },
+    //   manifest: {
+    //     icons: [
+    //       {
+    //         src: "/assets/icon/chrome-mask-512.png",
+    //         sizes: "512x512",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-mask-192.png",
+    //         sizes: "192x192",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //     shortcuts: [
+    //       {
+    //         name: "Demo",
+    //         short_name: "Demo",
+    //         url: "/demo/",
+    //         icons: [
+    //           {
+    //             src: "/assets/icon/guide-maskable.png",
+    //             sizes: "192x192",
+    //             purpose: "maskable",
+    //             type: "image/png",
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // },
+  },
+
+  navbarLayout: {
+      start: ["Brand"],
+      center: ["Links"],
+      end: ["Language", "Repo", "Outlook", "Search"],
+    },
+});
